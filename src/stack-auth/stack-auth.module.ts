05341155f66f642965ac4e2227f8b050d4a64e73
TristanDereigne <tristandereigne@gmail.com>
2025-11-24 17:22:51 +0100

    Staging (#9)
    * Add CODEOWNERS
    
    * feature&fix; add age and improve media handling in voyage detail page
    
    * migration bubble to new website
    
    * fix: make sequential operation because neon driver was experiencing issues for the bubble migration
    
    * fix: make sequential operation because neon driver was experiencing issues for the bubble migration v2
    
    * fix: make sequential operation because neon driver was experiencing issues for the bubble migration v3
    
    * fix: make sequential operation because neon driver was experiencing issues for the bubble migration v3.1
    
    * fix: make sequential operation because neon driver was experiencing issues for the bubble migration v3.5
    
    * fix: make sequential operation because neon driver was experiencing issues for the bubble migration v3.8
    
    * fix: make sequential operation because neon driver was experiencing issues for the bubble migration v3.9

--- src/stack-auth/stack-auth.module.ts
+++ src/stack-auth/stack-auth.module.ts
@@ -1,10 +1,14 @@
 import { Global, Module } from '@nestjs/common';
 import { StackAuthService } from './stack-auth.service';
 import { StackAuthCleanupService } from './stack-auth-cleanup.service';
+import { DatabaseModule } from '../database/database.module';
+import { StackAuthController } from './stack-auth.controller';
 
 @Global()
 @Module({
-  providers: [StackAuthService, StackAuthCleanupService],
+  imports: [DatabaseModule],
+  controllers: [StackAuthController],
+  providers: [StackAuthCleanupService, StackAuthService],
   exports: [StackAuthService],
 })
 export class StackAuthModule {}
